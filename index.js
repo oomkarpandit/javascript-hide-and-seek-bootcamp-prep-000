0bea843cd9ea274750558a11001e14cdc9f5a59a
Oomkar Pandit <oomkar@yahoo.com>
2018-08-19 06:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ for (let i=0;i<alist.length;i++){
 function deepestChild() {
   let node = document.getElementById('grand-node');
   let nextNode = node.children[0];
-console.log(node)
-console.log(nextNode)
+console.log(node.text)
+console.log(nextNode.text)
   while (nextNode) {
     node = nextNode;
     nextNode = node.children[0];
     console.log("inside")
-    console.log(node)
-    console.log(nextNode)
+    console.log(node.text)
+    console.log(nextNode.text)
     
   }
 
