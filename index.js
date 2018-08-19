5ec4e42013c7ba60caf5ca659ac6bb27cba7014e
Oomkar Pandit <oomkar@yahoo.com>
2018-08-19 06:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+//first test
+function getFirstSelector(selector1){
+return document.querySelector(selector1)
+}
+//second test
+function nestedTarget(){
+return document.querySelector('#nested .target')
+}
+//third test
+function increaseRankBy(n){
+const alist =document.querySelectorAll('.ranked-list li')
+for (let i=0;i<alist.length;i++){
+ alist[i].innerHTML=parseInt(alist[i].innerHTML)+parseInt(n)
+}
+}
+//fourth test
+function deepestChild(){
+  const fullchildlist=document.querySelectorAll('#grand-node div ')
+  console.log('length of list ' +fullchildlist.length)
+  for (let i=0;i<fullchildlist.length;){
+    console.log(fullchildlist[i].innerHTML)
+    
+  }
+}
