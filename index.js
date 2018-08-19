e21f1c19d9d3a4f10e072b1a9ff7d74efd33aab1
Oomkar Pandit <oomkar@yahoo.com>
2018-08-19 06:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function deepestChild(){
   console.log('length of list ' +fullchildlist.length)
   for (let i=0;i<fullchildlist.length;i++){
     console.log(fullchildlist[i].innerHTML)
-    
+    for (let a=0;a<fullchildlist[i].length;a++){
+      
+    }
     
   }
 }
