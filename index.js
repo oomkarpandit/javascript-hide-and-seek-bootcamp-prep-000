694d7665df5bcf403e7b787cc9a65ceb629c7a89
Oomkar Pandit <oomkar@yahoo.com>
2018-08-19 07:12:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+//first test
+function getFirstSelector(selector1){
+return document.querySelector(selector1)
+}
+//second test
+function nestedTarget(){
+return document.querySelector('#nested .target')
+}
+//third test
+function increaseRankBy(n){
+const alist =document.querySelectorAll('.ranked-list li')
+for (let i=0;i<alist.length;i++){
+ alist[i].innerHTML=parseInt(alist[i].innerHTML)+parseInt(n)
+}
+}
+//fourth test
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+   
+    
+  }
 
+  return node;
+}
